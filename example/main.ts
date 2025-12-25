70bd7956de99570e4ac6e3ec037e2991f7315f94
qiulongwen <qiulongwen@sensetime.com>
2025-12-25 16:46:37 +0800

    xiugai

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,5 @@
 // 引入整个组件库
 import '../src/main.ts';
 
+
 console.log('Example environment loaded');
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,9 @@
 export * from "@/components/counter/counter-component.ts";
 export * from "@/components/card/card-component.ts";
 export * from "@/components/button/button.ts";
+export * from "@/components/myButton/button.ts";
 
+import { componentRegistry } from "@a2ui/lit/ui";
+import { OrgChart } from "@/components/myButton/button.ts";
 
+componentRegistry.register("OrgChart", OrgChart, "org-chart");
